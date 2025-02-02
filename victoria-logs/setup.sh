8eea106a030e1c8d511a959046258719c1fa1a8b
Niki Dokovski <nickytd@gmail.com>
2025-02-02 21:39:18 +0100

    vl/operator: bump up operator version to v0.41.0

--- victoria-logs/setup.sh
+++ victoria-logs/setup.sh
@@ -7,7 +7,7 @@ set -o pipefail
 dir="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
 source $dir/../.include.sh
 
-version=${1:-"0.34.8"}
+version=${1:-"0.41.0"}
 namespace=${2:-"victoria-logs"}
 operator_chart_url=${3:-"https://github.com/VictoriaMetrics/helm-charts/releases/download/victoria-metrics-operator-$version/victoria-metrics-operator-$version.tgz"}
 
